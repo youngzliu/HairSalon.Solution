5ed5b75fd176fe9e73929f26d9e798399ca1492a
Young Liu <youngliu92@gmail.com>
2019-03-09 13:18:56 -0800

    add the ability to see multiple stylists.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
 
 namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
     [HttpGet("/")]
-    public ActionResult Index() { return View(); }
+    public ActionResult Index() {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
 
     [HttpGet("/stylist/new")]
     public ActionResult Form() { return View(); }
 
     [HttpPost("/stylist")]
-    public ActionResult Stylist(string firstName, string lastName, string phoneNumber, string email){
+    public ActionResult Create(string firstName, string lastName, string phoneNumber, string email){
       Stylist sty = new Stylist(firstName, lastName, phoneNumber, email, 1);
-      return View(sty);
+      return RedirectToAction("Index");
     }
+
+
   }
 }
