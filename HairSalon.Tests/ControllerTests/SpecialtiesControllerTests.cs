3e5687f10d4fa5bc3e588acbc95509af426719fb
Young Liu <youngliu92@gmail.com>
2019-03-12 16:07:12 -0700

    add formatting and some tests for SpecialtiesController.

--- HairSalon.Tests/ControllerTests/SpecialtiesControllerTests.cs
+++ HairSalon.Tests/ControllerTests/SpecialtiesControllerTests.cs
@@ -11,5 +11,42 @@ namespace HairSalon.Tests
     public SpecialtiesControllerTest(){
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=young_liu_test;";
     }
+
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True(){
+      SpecialtiesController controller = new SpecialtiesController();
+      ActionResult indexView = controller.Index();
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    }
+
+    [TestMethod]
+    public void Index_HasCorrectModelType_SpecialtyList(){
+      SpecialtiesController controller = new SpecialtiesController();
+      ViewResult indexView = controller.Index() as ViewResult;
+      var result = indexView.ViewData.Model;
+      Assert.IsInstanceOfType(result, typeof(List<Specialty>));
+    }
+
+    [TestMethod]
+    public void New_ReturnsCorrectView_True(){
+      SpecialtiesController controller = new SpecialtiesController();
+      ActionResult newView = controller.New();
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult(){
+      SpecialtiesController controller = new SpecialtiesController();
+      IActionResult view = controller.Create("Mullet");
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    }
+
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index(){
+      SpecialtiesController controller = new SpecialtiesController();
+      RedirectToActionResult actionResult = controller.Create("Mullet") as RedirectToActionResult;
+      string result = actionResult.ActionName;
+      Assert.AreEqual(result, "Index");
+    }
   }
 }
--- HairSalon/Controllers/SpecialtiesController.cs
+++ HairSalon/Controllers/SpecialtiesController.cs
@@ -6,6 +6,20 @@ namespace HairSalon.Controllers
 {
   public class SpecialtiesController : Controller
   {
+    [HttpGet("/specialties")]
+    public ActionResult Index() {
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      return View(allSpecialties);
+    }
 
+    [HttpGet("/specialties/new")]
+    public ActionResult New() { return View(); }
+
+    [HttpPost("/specialties")]
+    public ActionResult Create(string description){
+      Specialty specialty = new Specialty(description);
+      specialty.Save();
+      return RedirectToAction("Index");
+    }
   }
 }
